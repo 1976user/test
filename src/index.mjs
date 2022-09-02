32c81a08d561c30f6e4d131ce4ddb692e0c69f5f
1976 <1_976@mail.ru>
2022-09-02 17:11:33 +0400

    Added numbers computations.

--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,7 @@
 import { sum, avg } from './sub/index.mjs';
 
-console.log('Test!');
+const numbers = [1, 3, 5, 7, 9, 11];
+
+console.log(`Numbers: ${numbers.join(',')}`);
+console.log(`Sum: ${sum(...numbers)}`);
+console.log(`Avg: ${avg(...numbers)}`);
